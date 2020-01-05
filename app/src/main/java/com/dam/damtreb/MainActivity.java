afd96acd3b05607e2c0bcd6eed91e097ed281b93
BrunoARodriguez <brodriguez@frsf.utn.edu.ar>
2020-01-05 01:41:09 -0300

    commit arrancando con la interfaz

--- app/src/main/java/com/dam/damtreb/MainActivity.java
+++ app/src/main/java/com/dam/damtreb/MainActivity.java
@@ -3,12 +3,37 @@ package com.dam.damtreb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+private  Button btnLeerBilletes;
+private  Button btnAgregarUbicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLeerBilletes = (Button) findViewById(R.id.btnLeerBilletes);
+        btnAgregarUbicacion = (Button) findViewById(R.id.btnAgregarUbicacion);
+
+        btnLeerBilletes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"en desarrollo",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        btnAgregarUbicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"en desarrollo",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
+
+
